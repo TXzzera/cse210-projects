68f9bd76b29756e7881402dbebb840d6043a2b43
TXzzera <brunoteixeira.10@hotmail.com>
2025-03-14 20:14:19 -0300

    Exercises week 02 - Resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobtitle = "Software Engineer";
+        job1._company = "Oracle";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobtitle = "Data Engineer";
+        job2._company = "SalesForce";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+
+        Resume myresume = new Resume();
+        myresume._name = "Bruno Teixeira";
+        myresume._jobs.Add(job1);
+        myresume._jobs.Add(job2);
+        
+        myresume.Display();
+
     }
 }
---
+++ week02/Resumes/job.cs
@@ -0,0 +1,11 @@
+using System;
+ public class Job {
+            public string _jobtitle;
+            public string _company;
+            public int _startYear;
+            public int _endYear;
+
+            public void Display(){
+                Console.WriteLine($"{_jobtitle} ({_company}) {_startYear}-{_endYear}");
+            }
+        } 
