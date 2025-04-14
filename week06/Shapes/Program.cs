72745b69b14567e91526a85d227c74a77df60214
TXzzera <brunoteixeira.10@hotmail.com>
2025-04-14 13:05:19 -0300

    SHAPES

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square1 = new Square("blue", 5 );
+        shapes.Add(square1);
+
+        Rectangle rectangle1 = new Rectangle("yellow", 2.5, 3.5);
+        shapes.Add(rectangle1);
+
+        Circle circle1 = new Circle("red", 2);
+        shapes.Add(circle1);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area.ToString("F2")} cm");
+        }
+
     }
 }
